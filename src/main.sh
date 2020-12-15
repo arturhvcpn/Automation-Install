d5d9de09c0850e4af9e1a4a4e27e30fb9bd3ff5e
Artur Polo Norte <arturcastropolonorte@gmail.com>
2020-12-15 17:13:35 -0300

    :heavy_plus_sign: added script execution in main

--- src/main.sh
+++ src/main.sh
@@ -1,5 +1,9 @@
-function main {
-    sudo apt-get update && sudo apt-get upgrade -y
+#!/usr/bin/env bash
+function main {    
+    source scripts/installDefault.sh
+    source scripts/installNode.sh    
+    source scripts/installYarn.sh
+    source scripts/installProtractor.sh
 }
 
 main

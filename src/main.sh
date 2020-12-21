4887241a90a4f1a7c4a40d837071febca5d0f3dd
Artur Polo Norte <arturcastropolonorte@gmail.com>
2020-12-21 10:36:57 -0300

    :construction: change main

--- src/main.sh
+++ src/main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 function main {    
-    source scripts/installDefault.sh
+    source scripts/installZsh.sh
     source scripts/installNode.sh    
     source scripts/installYarn.sh
     source scripts/installProtractor.sh
